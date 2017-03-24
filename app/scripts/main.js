ea89cdb10d6b60f9ad2373c020d01bffc58cbe7c
SAPIENT\ibasak <ibasak@sapient.com>
2017-03-24 21:41:42 +0530

    Adding the tree view

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,35 @@
-console.log('\'Allo \'Allo!');
+var tree = [
+  {
+    text: "Button",
+    nodes: [
+      {
+        text: '<button type="button" onclick="alert("Hello world!")">Click Me!</button>',
+        nodes: [
+          {
+            text: "Grandchild 1"
+          },
+          {
+            text: "Grandchild 2"
+          }
+        ]
+      },
+      {
+        text: "Child 2"
+      }
+    ]
+  },
+  {
+    text: "Parent 2"
+  },
+  {
+    text: "Parent 3"
+  },
+  {
+    text: "Parent 4"
+  },
+  {
+    text: "Parent 5"
+  }
+];
+
+$('#tree').treeview({data: tree});
